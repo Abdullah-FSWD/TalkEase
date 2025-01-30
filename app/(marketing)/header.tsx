80b90d7cb239e01b47fe11f0429609e83e7e8855
abdullah-fswd <ansarimdabdullah794@gmail.com>
2025-01-30 12:48:25 +0530

    feat: authentication setup using clerk

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,49 @@
+"use client";
+
+import Image from "next/image";
+
+import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+import { Button } from "@/components/ui/button";
+
 export function Header() {
   return (
-    <header className="w-full h-20 border-2 boreder-slate-500 px-4">
-      Header
+    <header className=" h-20 w-full  border-b-2 boreder-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="mascot.svg" height={40} width={40} alt="Mascot" />
+          {/* <Image src="talkease.svg" height={100} width={100} alt="talkease" /> */}
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Talk Ease
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSwitchSessionUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              signUpFallbackRedirectUrl="/learn"
+              signUpForceRedirectUrl="/learn"
+            >
+              <Button size="lg" variant="ghost">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
     </header>
   );
 }
