faae3cab59eb2c5603a3e4b5355cd9d0c12ffb6d
abdullah-fswd <ansarimdabdullah794@gmail.com>
2025-02-04 12:58:44 +0530

    fix: fix for the uncompleted lessons with show completed

--- db/queries.ts
+++ db/queries.ts
@@ -67,6 +67,9 @@ export const getUnits = cache(async () => {
 
   const normalizedData = data.map((unit) => {
     const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      if (lesson.challenges.length === 0) {
+        return { ...lesson, completed: false };
+      }
       const allCompletedChaallenges = lesson.challenges.every((challenge) => {
         return (
           challenge.challengeProgress &&
